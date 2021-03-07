953ef841ac370548fd1a958ecfd4c3538cee42e7
jarekratajski <jratajski@gmail.com>
2021-03-07 11:52:33 +0100

    release 0.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
     `maven-publish`
     kotlin("jvm") version "1.4.31"
+    signing
+    id("org.jetbrains.dokka") version "0.10.1"
 }
 repositories {
     mavenCentral()
     jcenter()
 }
+
+val componentVersion = "0.1.3"
+
 dependencies {
     // When creating a sample extension, change this dependency to the detekt-api version you build against
     // e.g. io.gitlab.arturbosch.detekt:detekt-api:1.x.x
@@ -22,9 +27,96 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "pl.setblack"
             artifactId = "kure-potlin"
-            version = "0.1.3-SNAPSHOT"
+            version = componentVersion
 
             from(components["java"])
         }
     }
 }
+
+val ossrhUsername: String by project
+val ossrhPassword: String by project
+val signingKey: String? by project
+val signingPassword: String? by project
+
+val isRelease = !componentVersion.endsWith("-SNAPSHOT")
+
+
+val publications: PublicationContainer = (extensions.getByName("publishing") as PublishingExtension).publications
+
+signing {
+
+    if (signingKey != null && signingPassword != null) {
+        @Suppress("UnstableApiUsage")
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+
+    useGpgCmd()
+    sign(publications)
+}
+
+val dokka = tasks.named("dokka")
+
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    archiveClassifier.set("javadoc")
+    from(dokka)
+}
+
+publishing {
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+            name = "deploy"
+            println("isRelease: $isRelease")
+            url = if (isRelease) releasesRepoUrl else snapshotsRepoUrl
+            credentials {
+                username = System.getenv("OSSRH_USERNAME") ?: ossrhUsername
+                password = System.getenv("OSSRH_PASSWORD") ?: ossrhPassword
+            }
+        }
+    }
+
+    publications.forEach {
+        println("publication $it")
+    }
+
+    publications.withType<MavenPublication>().forEach {
+        it.apply {
+            if (isRelease) {
+                artifact(tasks["dokkaJar"])
+            }
+            artifact(tasks["kotlinSourcesJar"])
+
+            pom {
+                name.set("kure-potlin")
+                description.set("kure-potlin")
+                url.set("https://github.com/neeffect/kure-potlin")
+
+                scm {
+                    connection.set("scm:git:https://github.com/neeffect/kure-potlin.git")
+                    developerConnection.set("scm:git:http://github.com/jarekratajski/")
+                    url.set("https://github.com/neeffect/kure-potlin")
+                }
+
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://opensource.org/licenses/Apache-2.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("jarekratajski")
+                        name.set("Jarek Ratajski")
+                        email.set("jratajski@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+}
+
