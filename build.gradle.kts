9ec783e7b7c9c4a1e69fb874a153fcc40862dc01
krzykrucz <krzysiuk.66@gmail.com>
2021-03-16 19:49:32 +0100

    impure classes rule

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     // When creating a sample extension, change this dependency to the detekt-api version you build against
     // e.g. io.gitlab.arturbosch.detekt:detekt-api:1.x.x
     compileOnly("io.gitlab.arturbosch.detekt:detekt-api:1.15.0")
+    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.4.31")
     // When creating a sample extension, change this dependency to the detekt-test version you build against
     // e.g. io.gitlab.arturbosch.detekt:detekt-test:1.x.x
     testImplementation("io.gitlab.arturbosch.detekt:detekt-api:1.15.0")
---
+++ src/test/kotlin/io/gitlab/arturbosch/detekt/sample/extensions/NoOOClass.kt
@@ -0,0 +1,35 @@
+package io.gitlab.arturbosch.detekt.sample.extensions
+
+import io.gitlab.arturbosch.detekt.sample.extensions.rules.ObjectOrientedClassCode
+import io.gitlab.arturbosch.detekt.test.lint
+import org.assertj.core.api.Assertions.assertThat
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+class NoOOClass : Spek({
+
+
+    describe("object-oriented class code rule") {
+        val subject by memoized { ObjectOrientedClassCode() }
+
+        it("should find impure classes") {
+            val findings = subject.lint(impureClassesCode)
+            assertThat(findings).hasSize(3)
+        }
+    }
+})
+
+private const val impureClassesCode: String =
+    """
+        // impure
+        class OOClass
+        private class PrivateOOClass
+        abstract class AbstractClass
+
+        // pure
+        data class DataClass(val v: String)
+        inline class InlineClass(val v: String)
+        object Obj
+        interface Interface
+        sealed class SealedClass
+    """
