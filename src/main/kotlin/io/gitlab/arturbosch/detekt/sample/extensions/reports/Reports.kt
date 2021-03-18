bb386e339abfd4cf5236c2733965068dec0abf87
jarekratajski <jratajski@gmail.com>
2021-03-18 07:02:52 +0100

    version 2.1 prepared

--- src/main/kotlin/io/gitlab/arturbosch/detekt/sample/extensions/reports/Reports.kt
+++
@@ -1,9 +0,0 @@
-package io.gitlab.arturbosch.detekt.sample.extensions.reports
-
-import io.gitlab.arturbosch.detekt.api.Detektion
-import io.gitlab.arturbosch.detekt.sample.extensions.processors.fqNamesKey
-
-fun qualifiedNamesReport(detektion: Detektion): String? = detektion.getData(fqNamesKey)
-    ?.takeIf { it.isNotEmpty() }
-    ?.fold(StringBuilder()) { builder, fqName -> builder.append("$fqName\n") }
-    ?.toString()
